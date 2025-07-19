197fc792832c4bea16e834fcddf3763b88fd08a9
Ali Unal <0033484@gmail.com>
2025-07-19 13:30:11 +0300

    Auto registration added

--- Assets/ServiceLocator/Scripts/BaseService.cs
+++ Assets/ServiceLocator/Scripts/BaseService.cs
@@ -4,9 +4,14 @@ namespace AngryKoala.Services
 {
     public abstract class BaseService<T> : MonoBehaviour, IService where T : IService
     {
+        [SerializeField] protected bool _autoRegister = true;
+        
         protected virtual void Awake()
         {
-            Register();
+            if (_autoRegister)
+            {
+                Register();
+            }
         }
 
         public void Register()
